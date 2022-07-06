1b7460f13ebd1f3f479ec0e2fa5d22c40832b511
Alexander Cortez <acortez8988@gmail.com>
2022-07-06 13:02:47 -0400

    commit tree c48b393a621ff8f0664ab9471d99643956900d37

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberOfFeet){
+  if(numberOfFeet <= 400){ 
+  return `This one is on me!`;
+  } 
+   else if(numberOfFeet <= 2000) {
+  return `That will be twenty bucks.`;
+  } 
+   else if(numberOfFeet <= 2500) {
+    return `I will gladly take your thirty bucks.`
+}
+else return `No can do.`
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// condition ? exprIfTrue : exprIfFalse
+
+
+function switchOnCharmFromTip(tips){
+
+  
+
+  switch (tips) {
+    case `generous`:
+      return "Thank you so much.";
+      break;
+       case 'not as generous':
+        return "Thank you.";
+        break;
+        default:
+          return 'Bye.';
+          break;
+  }
 }
